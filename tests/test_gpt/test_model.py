18c290643787205ce064b276921972a42c4ed0f0
Nikhil Anand <nikhilanand@holylogin01.rc.fas.harvard.edu>
2024-09-18 15:45:52 -0400

    Add unit test for param counts

--- tests/test_gpt/test_model.py
+++ tests/test_gpt/test_model.py
@@ -22,8 +22,19 @@ def test_model_creation():
     assert isinstance(model.transformer['h'], nn.ModuleList)
     assert isinstance(model.transformer['ln_f'], nn.LayerNorm)
 
-def test_num_parameters():
-    pass
+def test_num_parameters(model):
+    def expected_parameters_head(d_model):
+        return 4*d_model**2+d_model
+
+    def expected_parameters_block(d_model):
+        return expected_parameters_head(d_model) + 8*d_model**2 + 5*d_model + 4*d_model # last is the 2 layer norms, each with 2 params
+
+    def expected_parameters_total(vocab_size, d_model, n_layer, context_length):
+        return n_layer*expected_parameters_block(d_model) + vocab_size*d_model + context_length*d_model + 2*d_model + d_model*vocab_size + vocab_size
+
+    actual = sum(p.numel() for p in model.parameters())
+    expected = expected_parameters_total(config.tokenizer.vocab_size, config.model.d_model, config.model.n_layer, config.model.context_length)
+    assert actual==expected
 
 def test_forward_pass():
     pass
